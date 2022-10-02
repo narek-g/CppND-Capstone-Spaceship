7e195c7cc01b5224bb5ca82efcc6729ceb203750
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-02 12:55:15 -0600

    Update renderer.h

--- src/renderer.h
+++ src/renderer.h
@@ -10,6 +10,7 @@ class Renderer {
   Renderer(const std::size_t screen_width, const std::size_t screen_height,
            const std::size_t grid_width, const std::size_t grid_height);
   ~Renderer();
+  void drawBlackHole(int centerX, int centerY, int radius);
 
   void Render(Snake const snake, SDL_Point const &food);
   void UpdateWindowTitle(int score, int fps);
