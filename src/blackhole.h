409665276170d0d53f35b4384889e4a549461dd3
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-07 12:27:35 -0600

    Update blackhole.h

--- src/blackhole.h
+++ src/blackhole.h
@@ -22,7 +22,25 @@ class BlackHole {
     int BHmass; 
     int BHradius; 
 
-    void getGravitationalPull(float &Fx, float &Fy, int x, int y); 
+    void getGravitationalPull(float &Fx, float &Fy, int x, int y){ 
+    // black hole 1 
+    float BH1Fx{0}; // update by reference in computeForece
+    float BH1Fy{0}; // update by reference in computeForce 
+    computeForce(BH1Fx, BH1Fy, x, y, blackHole.BH1x, blackHole.BH1y); 
+
+    // black hole 2
+    float BH2Fx{0}; // update by reference in computeForece
+    float BH2Fy{0}; // update by reference in computeForce 
+    computeForce(BH2Fx, BH2Fy, x, y, blackHole.BH2x, blackHole.BH2y); 
+
+    // black hole 3 
+    float BH3Fx{0}; // update by reference in computeForece
+    float BH3Fy{0}; // update by reference in computeForce 
+    computeForce(BH3Fx, BH3Fy, x, y, blackHole.BH3x, blackHole.BH3y); 
+
+    Fx = BH1Fx + BH2Fx + BH3Fx; 
+    Fy = BH1Fy + BH2Fy + BH3Fy; 
+   }
     void something(){
       std::cout << "Hello \n"; 
     }
