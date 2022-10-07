dcdecb878da29646d4cf5e5c5c4238ded0abd54f
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-07 11:47:24 -0600

    Update renderer.cpp

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -94,7 +94,7 @@ void Renderer::Render(Snake const snake, BlackHole blackHole, SDL_Point const &f
   block.x = static_cast<int>(snake.head_x) * block.w;
   block.y = static_cast<int>(snake.head_y) * block.h;
 
-  blackHole.getGravitationalPull(blackHole, BHFx, BHFy, block.x, block.y);
+  // blackHole.getGravitationalPull(blackHole, BHFx, BHFy, block.x, block.y);
 
   if (snake.alive) {
     SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF);
