1f7d526d8eb5477da7438fdbb366d440ddc06c2d
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-09 15:31:29 -0600

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -80,7 +80,7 @@ void Game::Update() {
     // PlaceFood();
     // Grow spaceship and increase speed.
     // spaceship.GrowBody();
-    spaceship.speed += 0.02;
+    //spaceship.speed += 0.02;
   }
 }
 
