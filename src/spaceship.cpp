24269f7662b8cc032276483648e3e148e8fb165e
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-10 03:42:23 -0600

    Update spaceship.cpp

--- src/spaceship.cpp
+++ src/spaceship.cpp
@@ -83,4 +83,5 @@ void SpaceShip::getNetForce(SpaceShip &spaceship, BlackHole &blackhole){
   BHFx = 0.0; 
   BHFy = 0.0; 
   blackhole.getGravitationalPull(blackhole, BHFx, BHFy, spaceship.head_x, spaceship.head_y);
+  std::cout << BHFx << " " << BHFy << "\n";
 }
