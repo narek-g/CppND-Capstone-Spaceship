99e71d4967f50559157f190657c9ea46f6680e01
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-10 03:30:26 -0600

    Update blackhole.cpp

--- src/blackhole.cpp
+++ src/blackhole.cpp
@@ -3,7 +3,7 @@
 #include <cmath> 
 
 
-void BlackHole::computeForce(BlackHole blackHole, float &Fx, float &Fy, const int x, const int y, int BHx, int BHy){
+void BlackHole::computeForce(BlackHole blackhole, float &Fx, float &Fy, const int x, const int y, int BHx, int BHy){
     float radiusSquared; 
     float G{6.67e-11}; 
     float F; 
@@ -11,26 +11,26 @@ void BlackHole::computeForce(BlackHole blackHole, float &Fx, float &Fy, const in
 
     radiusSquared = pow((BHx - x),2) + pow((BHy - y),2) ; 
     // F = (G*M*m)/radiusSquared;
-    F = (G*blackHole.BHmass * m)/radiusSquared;
+    F = (G*blackhole.BHmass * m)/radiusSquared;
     Fx = F*cos(atan2((BHy - y),(BHx - x))); 
     Fy = F*sin(atan2((BHy - y),(BHx - x))); 
    }
 
-void BlackHole::getGravitationalPull(BlackHole blackHole, float &Fx, float &Fy, const int x, const int y){ 
+void BlackHole::getGravitationalPull(BlackHole blackhole, float &Fx, float &Fy, const int x, const int y){ 
     // black hole 1 
     BH1Fx = 0; // update by reference in computeForece
     BH1Fy = 0; // update by reference in computeForce 
-    computeForce(blackHole, BH1Fx, BH1Fy, x, y, blackHole.BH1x, blackHole.BH1y); 
+    computeForce(blackhole, BH1Fx, BH1Fy, x, y, blackhole.BH1x, blackhole.BH1y); 
 
     // black hole 2
     BH2Fx = 0; // update by reference in computeForece
     BH2Fy = 0; // update by reference in computeForce 
-    computeForce(blackHole, BH2Fx, BH2Fy, x, y, blackHole.BH2x, blackHole.BH2y); 
+    computeForce(blackhole, BH2Fx, BH2Fy, x, y, blackhole.BH2x, blackhole.BH2y); 
 
     // black hole 3 
     BH3Fx = 0; // update by reference in computeForece
     BH3Fy = 0; // update by reference in computeForce 
-    computeForce(blackHole, BH3Fx, BH3Fy, x, y, blackHole.BH3x, blackHole.BH3y); 
+    computeForce(blackhole, BH3Fx, BH3Fy, x, y, blackhole.BH3x, blackhole.BH3y); 
 
     Fx = BH1Fx + BH2Fx + BH3Fx; 
     Fy = BH1Fy + BH2Fy + BH3Fy; 
