58dd27c42507607c75e57141d47831c5b286fa7b
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-10-28 17:34:49 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
   constexpr std::size_t kFramesPerSecond{30};
-  constexpr std::size_t kMsPerFrame{500 / kFramesPerSecond};
+  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
   constexpr std::size_t kScreenHeight{640};
   constexpr std::size_t kGridWidth{32};
